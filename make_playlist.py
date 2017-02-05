d8637ac66af4146a958d130e6666dd924a08d0a9
Kumassy <kumassyii@gmail.com>
2017-02-05 23:42:23 +0900

    csv read

--- make_playlist.py
+++ make_playlist.py
@@ -3,18 +3,17 @@
 from gmusicapi import Mobileclient
 import os
 import json
+import pandas as pd
 
-api = Mobileclient()
-api.login(os.environ['GMUSIC_USER'], os.environ['GMUSIC_PW'], Mobileclient.FROM_MAC_ADDRESS)
-
-search_result = api.search("サクラあっぱれーしょん")
-
-playlist = api.create_playlist("てすとてすと")
-# print(search_result['song_hits'])
-# print(search_result['song_hits'][0])
-# print(search_result['song_hits'][0]['track']['nid'])
-# api.add_songs_to_playlist(playlist, search_result['song_hits'][0]['track']['nid'])
-api.add_songs_to_playlist(playlist, search_result['song_hits'][0]['track']['storeId'])
-
+# api = Mobileclient()
+# api.login(os.environ['GMUSIC_USER'], os.environ['GMUSIC_PW'], Mobileclient.FROM_MAC_ADDRESS)
+#
+# search_result = api.search("サクラあっぱれーしょん")
+#
+# playlist = api.create_playlist("てすとてすと")
+# api.add_songs_to_playlist(playlist, search_result['song_hits'][0]['track']['storeId'])
 
 # print(json.dumps(search_result['song_hits']))
+
+df = pd.read_csv('anison.csv', quotechar='"', escapechar="\\")
+print df
